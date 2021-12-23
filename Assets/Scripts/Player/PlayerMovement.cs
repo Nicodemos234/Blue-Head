91f749b2a675d869e7a436c939a0d83714afbb9d
nicodemos234 <nicodemos54324@gmail.com>
2021-12-23 19:23:28 -0300

    Add witcher animations

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,11 +7,13 @@ public class PlayerMovement : MonoBehaviour
   Rigidbody2D playerRigidbody;
   public float playerVelocity = 1.7f;
   Vector2 movement;
+  Animator anim;
 
   // Start is called before the first frame update
   void Start()
   {
     playerRigidbody = GetComponent<Rigidbody2D>();
+    anim = GetComponent<Animator>();
   }
 
   // Update is called once per frame
@@ -19,6 +21,8 @@ public class PlayerMovement : MonoBehaviour
   {
     movement.y = Input.GetAxis("Vertical");
     movement.x = Input.GetAxis("Horizontal");
+    anim.SetFloat("X", movement.x);
+    anim.SetFloat("Y", movement.y);
   }
 
   private void FixedUpdate()
